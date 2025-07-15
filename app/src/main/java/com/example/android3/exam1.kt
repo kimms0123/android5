40d35c48d2f447f5715b7eb22b678d9c0c24ee97
kimms0123 <161434331+kimms0123@users.noreply.github.com>
2025-07-15 22:07:34 +0900

    SQLite 직접풀어보기

--- app/src/main/java/com/example/android3/exam1.kt
+++ app/src/main/java/com/example/android3/exam1.kt
@@ -13,6 +13,38 @@ import androidx.appcompat.app.AppCompatActivity
 /*
     1. SQLite 프로그래밍
     [SQLite 관련 클래스 동작]
+        - 앱 데이터(회원 정보, 메모, 게시판)를 로컬에 저장가능
+    [SQLite]
+        - 앱 전용 데이터베이스 파일을 /data/data/앱패키지/databases/에 생성
+        - 앱마다 독립적인 데이터베이스 파일로 운영
+        - SQL 문법 그대로 사용 가능
+    [기본 동작 흐름]
+        1. SQLiteOpenHelper 상속 클래스 작성
+            - onCreate() : DB 처음 생성될 때 호출되어 테이블 갱신
+            - onUpgrade() : DB 버전이 변경될 때 호출되어 테이블 갱신
+        2. SQLiteDataBase 객체 열기
+            - writeableDatabase : 쓰기/읽기 가능
+            - readableDatabase : 읽기 전용
+        3. SQL 쿼리 실행
+            - execSQL() : INSERT, UPDATE, DELETE 테이블 생성 등 실행
+            - rawQuery() : SELECT 쿼리 실행 후 Cursor로 결과 받기
+        4. 결과 처리 및 DB 닫기
+            - close() : DB 닫기
+    [SQLite 관련 클래스의 동작]
+        - SQLiteOpenHelper  : 데이터베이스 생성과 버전 관리 담당. (onCreate(), onUpgrade() 오버라이딩)
+        - SQLiteDatabase    : 데이터베이스를 열고, 쿼리를 실행하는 객체.(execSQL(), rawQuery() 제공)
+        - Cursor            : rawQuery() 결과로 반환된 테이블 형태 데이터를 탐색하는 객체
+        - ContentValues     : key-value 형태로 데이터를 담아 insert/update에 사용 가능(옵션)
+
+              | 클래스 |              | 메서드 |                    | 설명 |             |
+        | :--------------- | :------------------------ | :----------------------------|
+        | SQLiteOpenHelper | `getWritableDatabase()`   | 읽기/쓰기 가능한 DB 객체 반환    |
+        | SQLiteOpenHelper | `getReadableDatabase()`   | 읽기 전용 DB 객체 반환          |
+        | SQLiteDatabase   | `execSQL()`               | SQL 실행 (결과 반환 X)         |
+        | SQLiteDatabase   | `rawQuery()`              | SELECT 쿼리 실행 후 Cursor 반환 |
+        | Cursor           | `moveToNext()`            | 다음 행으로 이동                |
+        | Cursor           | `getString()`, `getInt()` | 현재 행에서 컬럼 값 가져오기      |
+
 
     [SQLite 관련 클래스 및 인터페이스와 메소드]
 
@@ -27,6 +59,8 @@ class exam1 : AppCompatActivity() {
     lateinit var btnInit : Button
     lateinit var btnInsert : Button
     lateinit var btnSelect : Button
+    lateinit var btnUpdate : Button
+    lateinit var btnDelete : Button
     lateinit var sqlDB : SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +76,8 @@ class exam1 : AppCompatActivity() {
         btnInit = findViewById(R.id.btnInit)
         btnSelect = findViewById(R.id.btnSelect)
         btnInsert = findViewById(R.id.btnInsert)
+        btnUpdate = findViewById(R.id.btnUpdate)
+        btnDelete = findViewById(R.id.btnDelete)
 
         myHelper = myDBHelper(this)
         btnInit.setOnClickListener {
@@ -53,6 +89,7 @@ class exam1 : AppCompatActivity() {
             sqlDB = myHelper.writableDatabase
             sqlDB.execSQL("INSERT INTO groupTBL VALUES ('" + edtName.text.toString() + "' , "
                     + edtNumber.text.toString() + ");")
+            btnSelect.callOnClick()
             sqlDB.close()
             Toast.makeText(applicationContext, "입력됨", Toast.LENGTH_SHORT).show()
         }
@@ -74,6 +111,20 @@ class exam1 : AppCompatActivity() {
 
             cursor.close()
             sqlDB.close()
+        }
+        btnUpdate.setOnClickListener {
+            sqlDB = myHelper.writableDatabase
+            sqlDB.execSQL("UPDATE groupTBL SET gNumber = ${edtNumber.text} WHERE gName = '${edtName.text}';")
+            sqlDB.close()
+            btnSelect.callOnClick()
+            Toast.makeText(applicationContext, "수정됨", Toast.LENGTH_SHORT).show()
+        }
+        btnDelete.setOnClickListener {
+            sqlDB = myHelper.writableDatabase
+            sqlDB.execSQL("DELETE FROM groupTBL WHERE gName = '${edtName.text}';")
+            btnSelect.callOnClick()
+            sqlDB.close()
+            Toast.makeText(applicationContext, "삭제됨", Toast.LENGTH_SHORT).show()
         }
 
     }
